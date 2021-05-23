b8603c89c66b6bb219fd68e706f744c20b6db8f7
Alex Knopach <alexanderknopach@gmail.com>
2021-05-23 03:44:59 +0300

    feat: implement dateHelper

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,69 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    
+    if (monthNumber <= 0 || monthNumber > 12) {
+        return nil;
+    }
+    
+    NSDateComponents *dateComponents = [NSDateComponents new];
+    [dateComponents setMonth:monthNumber];
+    NSDate *date = [[NSCalendar currentCalendar] dateFromComponents:dateComponents];
+    
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    [dateFormatter setDateFormat:@"MMMM"];
+   
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    
+    NSDateFormatter *initialFormatter = [NSDateFormatter new];
+    [initialFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+    
+    NSDate *inputDate = [initialFormatter dateFromString:date];
+    
+    NSDateFormatter *resultFormatter = [NSDateFormatter new];
+    [resultFormatter setDateFormat:@"dd"];
+        
+    return [[resultFormatter stringFromDate:inputDate] integerValue];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    [dateFormatter setDateFormat:@"E"];
+    NSLocale *rusLocale = [NSLocale localeWithLocaleIdentifier:@"ru_RU"];
+    [dateFormatter setLocale:rusLocale];
+    
+    
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    [currentCalendar setLocale:[NSLocale currentLocale]];
+
+    NSDateComponents *dateComponents = [currentCalendar components:NSCalendarUnitWeekOfMonth fromDate:[NSDate now]];
+    
+    NSInteger thisWeek = [dateComponents weekOfMonth];
+
+    NSDateComponents *anotherComponents = [currentCalendar components:NSCalendarUnitWeekOfMonth fromDate:date];
+    
+    NSInteger weekToCompare = [anotherComponents weekOfMonth];
+    
+    if (thisWeek == weekToCompare) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 @end
