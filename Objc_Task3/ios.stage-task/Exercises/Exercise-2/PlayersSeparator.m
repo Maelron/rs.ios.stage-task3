522c92a2a1dc66cee9ac33c0f0c5dd2daaca8e1b
Alex Knopach <alexanderknopach@gmail.com>
2021-05-23 04:29:46 +0300

    feat: implement dividePlayersIntoTeams: method

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,25 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    if (ratingArray.count < 3) {
+        return 0;
+    }
+    
+    NSInteger count = 0;
+
+    for (int i = 0; i < ratingArray.count; i++) {
+        for (int j = i + 1; j < ratingArray.count; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if ((ratingArray[i].integerValue < ratingArray[j].integerValue && ratingArray[j].integerValue < ratingArray[k].integerValue) ||
+                    (ratingArray[i].integerValue > ratingArray[j].integerValue && ratingArray[j].integerValue > ratingArray[k].integerValue)) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
